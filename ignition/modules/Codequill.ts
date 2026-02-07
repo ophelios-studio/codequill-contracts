908bdc0b80ec9eca0dcb04dac901649719be50aa
dtucker <davidt2003@msn.com>
2026-02-07 12:52:29 -0500

    feat: adjust dependency order of contracts

--- ignition/modules/Codequill.ts
+++ ignition/modules/Codequill.ts
@@ -2,11 +2,13 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("CodeQuill", (m) => {
     const admin = m.getAccount(0);
+    const workspace = m.contract("CodeQuillWorkspaceRegistry", [admin]);
     const delegation = m.contract("CodeQuillDelegation", [admin]);
-    const registry = m.contract("CodeQuillRegistry", [admin, delegation]);
-    const snapshot = m.contract("CodeQuillSnapshotRegistry", [admin, registry, delegation]);
-    const backup = m.contract("CodeQuillBackupRegistry", [admin, registry, delegation, snapshot]);
-    const release = m.contract("CodeQuillReleaseRegistry", [admin, registry, delegation, snapshot]);
-    const attestation = m.contract("CodeQuillAttestationRegistry", [admin, registry, delegation, release]);
-    return { delegation, registry, snapshot, attestation, backup, release };
+    const repository = m.contract("CodeQuillRepositoryRegistry", [admin, delegation, workspace]);
+    const snapshot = m.contract("CodeQuillSnapshotRegistry", [admin, repository, workspace, delegation]);
+    const backup = m.contract("CodeQuillBackupRegistry", [admin, repository, workspace, delegation, snapshot]);
+    const release = m.contract("CodeQuillReleaseRegistry", [admin, repository, workspace, delegation, snapshot]);
+    const attestation = m.contract("CodeQuillAttestationRegistry", [admin, workspace, delegation, release]);
+
+    return { workspace, delegation, repository, snapshot, backup, release, attestation };
 });
